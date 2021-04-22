39f097a07e2d6161bfb89a0c7cffada7dee6e7f2
Turcanu <rturcanu@techteam.com>
2021-04-22 14:03:36 +0300

    Added: config.properties

--- src/main/java/com/stefanini/taskmanager/config/DataSourceProvider.java
+++ src/main/java/com/stefanini/taskmanager/config/DataSourceProvider.java
@@ -1,19 +1,34 @@
 package com.stefanini.taskmanager.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-
+import org.apache.logging.log4j.LogManager;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DataSourceProvider {
 
     private final static BasicDataSource mysqlDataSource = new BasicDataSource();
+
     static {
-        mysqlDataSource.setUrl("jdbc:mysql://localhost/myapplication");
-        mysqlDataSource.setUsername("root");
-        mysqlDataSource.setPassword("Rainb0vv@446571");
-        mysqlDataSource.setMinIdle(4);
-        mysqlDataSource.setMaxOpenPreparedStatements(100);
+        try {
+            Properties properties = new Properties();
+            properties.load(new FileInputStream("src/main/resources/config.properties"));
+
+            mysqlDataSource.setUrl(properties.getProperty("url"));
+            mysqlDataSource.setUsername(properties.getProperty("user"));
+            mysqlDataSource.setPassword(properties.getProperty("password"));
+            mysqlDataSource.setMinIdle(4);
+            mysqlDataSource.setMaxOpenPreparedStatements(100);
+
+        } catch (FileNotFoundException e) {
+            LogManager.getRootLogger().error("No such properties file");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static Connection getMysqlConnection() throws SQLException {
