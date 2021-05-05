39d7117e4fe81016dc1d143c56fad94330327438
Turcanu <rturcanu@techteam.com>
2021-05-05 15:03:10 +0300

    Refactor Application to use thread pool

--- dao-service/src/main/java/com/stefanini/taskmanager/service/agent/NotificationAgent.java
+++ dao-service/src/main/java/com/stefanini/taskmanager/service/agent/NotificationAgent.java
@@ -1,75 +1,75 @@
-package com.stefanini.taskmanager.service.agent;
-
-import com.stefanini.taskmanager.entities.Task;
-import com.stefanini.taskmanager.entities.User;
-import com.stefanini.taskmanager.util.annotation.NotifyWhenPersisting;
-import com.stefanini.taskmanager.util.sender.Sender;
-import com.stefanini.taskmanager.util.sender.impl.EmailSender;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-
-@Aspect
-public class NotificationAgent {
-
-    Sender sender = new EmailSender("radu.turcanu@extendaretail.com");
-
-    @Around("within(com.stefanini.taskmanager.service.impl.UserServiceImpl)")
-    public Object notifyTaskAdding(ProceedingJoinPoint pjp) throws Throwable {
-
-        Object retValue;
-
-        switch (pjp.toString()) {
-            case "call(AbstractEntity com.stefanini.taskmanager.dao.UserDao.create(AbstractEntity))":
-
-                retValue = pjp.proceed();
-
-                if (retValue.getClass().isAnnotationPresent(NotifyWhenPersisting.class))
-                    notifyUserCreating((User)retValue);
-                break;
-
-            case "execution(boolean " +
-                    "com.stefanini.taskmanager.service.impl.UserServiceImpl.addTask(Task, String))":
-
-                Task task = (Task)pjp.getArgs()[0];
-                String userName = (String)pjp.getArgs()[1];
-
-                retValue = pjp.proceed();
-
-                if ((boolean) retValue &&
-                        task.getClass().isAnnotationPresent(NotifyWhenPersisting.class))
-                    notifyTaskAssigning(task, userName);
-                break;
-
-            case "execution(User com.stefanini.taskmanager.service.impl.UserServiceImpl" +
-                    ".createUserAndAddTask(User, Task))":
-                userName = ((User)pjp.getArgs()[0]).getUserName();
-                task = (Task)pjp.getArgs()[1];
-
-                retValue = pjp.proceed();
-
-                if (retValue != null && retValue.getClass()
-                        .isAnnotationPresent(NotifyWhenPersisting.class))
-                    notifyTaskAssigning(task, userName);
-                break;
-
-            default:
-                retValue = pjp.proceed();
-        }
-        return retValue;
-    }
-
-    private void notifyUserCreating(User user) {
-        String message = String.format("User %s / %s identified by %s has been created!",
-                user.getFirstName(), user.getLastName(), user.getUserName());
-
-        sender.send("New user created!", message);
-    }
-
-    private void notifyTaskAssigning(Task task, String userName) {
-        String message = String.format("Task %s has been assigned to %s!",
-                task.getTitle(), userName);
-
-        sender.send("Task assigned!", message);
-    }
-}
+//package com.stefanini.taskmanager.service.agent;
+//
+//import com.stefanini.taskmanager.entities.Task;
+//import com.stefanini.taskmanager.entities.User;
+//import com.stefanini.taskmanager.util.annotation.NotifyWhenPersisting;
+//import com.stefanini.taskmanager.util.sender.Sender;
+//import com.stefanini.taskmanager.util.sender.impl.EmailSender;
+//import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.annotation.Aspect;
+//
+//@Aspect
+//public class NotificationAgent {
+//
+//    Sender sender = new EmailSender("radu.turcanu@extendaretail.com");
+//
+//    @Around("within(com.stefanini.taskmanager.service.impl.UserServiceImpl)")
+//    public Object notifyTaskAdding(ProceedingJoinPoint pjp) throws Throwable {
+//
+//        Object retValue;
+//
+//        switch (pjp.toString()) {
+//            case "call(AbstractEntity com.stefanini.taskmanager.dao.UserDao.create(AbstractEntity))":
+//
+//                retValue = pjp.proceed();
+//
+//                if (retValue.getClass().isAnnotationPresent(NotifyWhenPersisting.class))
+//                    notifyUserCreating((User)retValue);
+//                break;
+//
+//            case "execution(boolean " +
+//                    "com.stefanini.taskmanager.service.impl.UserServiceImpl.addTask(Task, String))":
+//
+//                Task task = (Task)pjp.getArgs()[0];
+//                String userName = (String)pjp.getArgs()[1];
+//
+//                retValue = pjp.proceed();
+//
+//                if ((boolean) retValue &&
+//                        task.getClass().isAnnotationPresent(NotifyWhenPersisting.class))
+//                    notifyTaskAssigning(task, userName);
+//                break;
+//
+//            case "execution(User com.stefanini.taskmanager.service.impl.UserServiceImpl" +
+//                    ".createUserAndAddTask(User, Task))":
+//                userName = ((User)pjp.getArgs()[0]).getUserName();
+//                task = (Task)pjp.getArgs()[1];
+//
+//                retValue = pjp.proceed();
+//
+//                if (retValue != null && retValue.getClass()
+//                        .isAnnotationPresent(NotifyWhenPersisting.class))
+//                    notifyTaskAssigning(task, userName);
+//                break;
+//
+//            default:
+//                retValue = pjp.proceed();
+//        }
+//        return retValue;
+//    }
+//
+//    private void notifyUserCreating(User user) {
+//        String message = String.format("User %s / %s identified by %s has been created!",
+//                user.getFirstName(), user.getLastName(), user.getUserName());
+//
+//        sender.send("New user created!", message);
+//    }
+//
+//    private void notifyTaskAssigning(Task task, String userName) {
+//        String message = String.format("Task %s has been assigned to %s!",
+//                task.getTitle(), userName);
+//
+//        sender.send("Task assigned!", message);
+//    }
+//}
--- taskmanager/src/main/java/com/stefanini/taskmanager/Application.java
+++ taskmanager/src/main/java/com/stefanini/taskmanager/Application.java
@@ -1,7 +1,19 @@
 package com.stefanini.taskmanager;
 
+import com.stefanini.taskmanager.entities.Task;
+import com.stefanini.taskmanager.entities.User;
+import com.stefanini.taskmanager.operation.user.AddTask;
+import com.stefanini.taskmanager.operation.user.CreateUser;
+import com.stefanini.taskmanager.operation.user.ShowAllUsers;
+import com.stefanini.taskmanager.operation.user.ShowUserTasks;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 
 public class Application {
@@ -14,7 +26,41 @@ public class Application {
 
         logger.trace("Application started.");
 
-        System.out.println("Please, enter users first name:");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Please, enter users first name:");
+        String firstName = scanner.next();
+
+        System.out.println("Please, enter users last name:");
+        String lastName = scanner.next();
+
+        System.out.println("Please, enter users user name:");
+        String userName = scanner.next();
+
+        System.out.println("Please, enter task title:");
+        String title = scanner.next();
+
+        System.out.println("Please, enter task description:");
+        String description = scanner.next();
+
+        ExecutorService service = Executors.newFixedThreadPool(4);
+        try {
+            service.submit(new CreateUser(new User(firstName, lastName, userName))).get();
+
+            Future addTask = service.submit(new AddTask(userName, new Task(title, description)));
+
+            service.execute(new ShowAllUsers());
+
+            addTask.get();
+
+            service.submit(new ShowUserTasks(userName)).get();
+        }
+        catch (InterruptedException | ExecutionException e) {
+            logger.error(e.getMessage());
+        }
+
+        service.shutdownNow();
+
+        logger.trace("Application finished");
     }
 }
